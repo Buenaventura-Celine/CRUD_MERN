1fd3686d5d5651aa3ea90b791d2cb031672b6592
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-08-14 13:43:42 +0800

    Created a Create Todo Form

--- mern-crud/src/CreateTodo.js
+++ mern-crud/src/CreateTodo.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import  { useForm } from "react-hook-form";
 
-export const CreateTodo = () =>{
-    return <div className="container">Create Todo</div>
-}
+export const CreateTodo = () => {
+    const { register, handleSubmit} = useForm();
+
+    const onSubmit = handleSubmit((data) =>{
+        alert(JSON.stringify(data));
+    });
+    return (
+        <div className="container">
+            <div className="mt-3">
+                <h3> Create Todo Item</h3>
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="text">Text:</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="text"
+                            id="text"
+                            autoComplete="off"
+                            {...register("text", {
+                                required: "Required",
+                            })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">
+                            Create Todo
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
